bcd2ebe2f4f0058639e323a8adbeca1ced404709
marlon-erler <viridian035@gmail.com>
2024-10-22 15:16:29 +0200

    add configs, reformat

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import "./themes/aero/index.css";
 import "./themes/standard/index.css";
 
 export enum Theme {
-  Standard = "standard",
-  Aero = "aero",
+    Standard = "standard",
+    Aero = "aero",
 }
 
 export function setTheme(theme: Theme): void {
-  document.body.setAttribute("theme", theme);
+    document.body.setAttribute("theme", theme);
 }
